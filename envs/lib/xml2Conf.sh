b4394857fb6cc246877453ae84ffbc04973df183
linhta71 <linhtamy71@gmail.com>
2021-12-19 13:43:56 -0700

    first commit

---
+++ envs/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/linhta/Documents/genome/imageseg/envs/lib"
+XML2_LIBS="-lxml2 -L/Users/linhta/Documents/genome/imageseg/envs/lib -lz -L/Users/linhta/Documents/genome/imageseg/envs/lib -llzma -lpthread -L/Users/linhta/Documents/genome/imageseg/envs/lib -liconv -L/Users/linhta/Documents/genome/imageseg/envs/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/linhta/Documents/genome/imageseg/envs/include/libxml2 -I/Users/linhta/Documents/genome/imageseg/envs/include"
+MODULE_VERSION="xml2-2.9.12"
+
